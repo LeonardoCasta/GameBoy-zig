0de0b9e389f98b1d930111326310e36d35f54d1e
LeonardoCasta <castagnaro.leonardo@gmail.com>
2026-05-18 20:16:45 -0400

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const Io = std.Io;
+const rl = @cImport({
+    @cInclude("raylib.h");
+});
+const GameBoy = @import("GameBoy");
+//init: std.process.Init
+pub fn main() !void {
+
+}

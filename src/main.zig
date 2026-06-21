865bb9955a827dc5b0ef4a8714b04a14402aa98e
LeonardoCasta <castagnaro.leonardo@gmail.com>
2026-06-21 15:45:15 -0400

    adding games

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,21 @@
 const std = @import("std");
 const Io = std.Io;
-const rl = @cImport({
+const ray = @cImport({
     @cInclude("raylib.h");
 });
 const GameBoy = @import("GameBoy");
 //init: std.process.Init
-pub fn main() !void {
+pub fn main() void {
+    // Hide all raylib log messages
+        ray.SetTraceLogLevel(ray.LOG_NONE);
 
+        ray.InitWindow(800, 450, "raylib [core] example");
+
+        while (!ray.WindowShouldClose()) {
+            ray.BeginDrawing();
+            ray.ClearBackground(ray.RAYWHITE);
+            ray.EndDrawing();
+        }
+
+        ray.CloseWindow();
 }
